f2aa2d90482cba97c59b1e10e3f0da3ab8e7a95a
SuneelFreimuth <suneelfreimuth1@gmail.com>
2020-08-09 21:26:31 -0700

    Actual minesweeper commit, but not done

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./stylesheets/App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Sun, Train, Snake, Minesweeper } from "./components";
+
+export default function App() {
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route path="/2train" component={Train} />
+					<Route path="/snake" component={Snake} />
+					<Route path="/minesweeper" component={Minesweeper} />
+					<Route exact path="/" component={Sun} />
+				</Switch>
+			</div>
+		</Router>
+	);
+}
